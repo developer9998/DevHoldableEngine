678e9ff54bb53ab8b11335ae6b94c0181082b53e
dev9998 <81720436+developer9998@users.noreply.github.com>
2023-02-13 21:22:27 -0330

    Initial commit

---
+++ Code/DevHoldable.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using GorillaLocomotion;
+using System.Linq;
+using Steamworks;
+
+namespace DevHoldableEngine
+{
+    public class DevHoldable : HoldableObject
+    {
+        public bool InHand;
+        public bool InLeftHand;
+        public bool PickUp;
+        public Rigidbody Rigidbody;
+
+        public float Distance = 0.2f;
+        public float ThrowForce = 1.75f;
+
+        public virtual void OnGrab(bool isLeft)
+        {
+            if (Rigidbody != null)
+            {
+                Rigidbody.isKinematic = true;
+                Rigidbody.useGravity = false;
+            }
+        }
+
+        public virtual void OnDrop(bool isLeft)
+        {
+            if (Rigidbody != null)
+            {
+                GorillaVelocityEstimator gorillaVelocityEstimator = (isLeft ? Player.Instance.leftHandTransform.GetComponentInChildren<GorillaVelocityEstimator>() : Player.Instance.rightHandTransform.GetComponentInChildren<GorillaVelocityEstimator>()) ?? null;
+
+                if (gorillaVelocityEstimator != null)
+                {
+                    Rigidbody.isKinematic = false;
+                    Rigidbody.useGravity = true;
+                    Rigidbody.velocity = (gorillaVelocityEstimator.linearVelocity * ThrowForce) + Player.Instance.GetComponent<Rigidbody>().velocity;
+                    Rigidbody.angularVelocity = gorillaVelocityEstimator.angularVelocity;
+                }
+            }
+        }
+
+        public void Update()
+        {
+            InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(CommonUsages.grip, out float left);
+            InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.grip, out float right);
+
+            bool leftGrip = left >= 0.65f;
+            bool rightGrip = right >= 0.65f;
+
+            if (PickUp && leftGrip && Vector3.Distance(Player.Instance.leftHandTransform.position, transform.position) < Distance && !InHand && EquipmentInteractor.instance.leftHandHeldEquipment == null)
+            {
+                // Hold logic
+                InLeftHand = true;
+                InHand = true;
+                transform.SetParent(GorillaTagger.Instance.offlineVRRig.leftHandTransform.parent);
+
+                // Other logic
+                GorillaTagger.Instance.StartVibration(true, 0.1f, 0.05f);
+                EquipmentInteractor.instance.leftHandHeldEquipment = this;
+
+                // Callback
+                OnGrab(true);
+            }
+            else if (!leftGrip && InHand && InLeftHand)
+            {
+                // Drop logic
+                InLeftHand = true;
+                InHand = false;
+                transform.SetParent(null);
+
+                // Other logic
+                GorillaTagger.Instance.StartVibration(true, 0.1f, 0.05f);
+                EquipmentInteractor.instance.leftHandHeldEquipment = null;
+
+                // Callback
+                OnDrop(true);
+            }
+
+            if (PickUp && rightGrip && Vector3.Distance(Player.Instance.rightHandTransform.position, transform.position) < Distance && !InHand && EquipmentInteractor.instance.rightHandHeldEquipment == null)
+            {
+                // Hold logic
+                InLeftHand = false;
+                InHand = true;
+                transform.SetParent(GorillaTagger.Instance.offlineVRRig.rightHandTransform.parent);
+
+                // Other logic
+                GorillaTagger.Instance.StartVibration(false, 0.1f, 0.05f);
+                EquipmentInteractor.instance.rightHandHeldEquipment = this;
+
+                // Callback
+                OnGrab(false);
+            }
+            else if (!rightGrip && InHand && !InLeftHand)
+            {
+                // Drop logic
+                InLeftHand = false;
+                InHand = false;
+                transform.SetParent(null);
+
+                // Other logic
+                GorillaTagger.Instance.StartVibration(false, 0.1f, 0.05f);
+                EquipmentInteractor.instance.rightHandHeldEquipment = null;
+
+                // Callback
+                OnDrop(false);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # DevHoldableEngine
- A physics based holdable system for Gorilla Tag
+DevHoldableEngine is a physics based holdable system for Gorilla Tag.
+
+## Item Logic
+Grabbing and releasing items is super easy. <br />
+Go to the item you want to pick up, put one of your hands over that item, and then hold down grip. When doing so you will hold the item, but when you release grip, it will also release the item you're holding. <br />
+The item acts like an ingame item too, for instance if you have a custom item in your right hand, you're not able to pick up any others with that hand until you release that custom item. <br />
+
+## Item Customization
+You can customize a ton of information relating to custom items, grab distance, throw force, etc. <br />
+Here's a table of some of them: <br />
+| Variable Name  | Action                                                              |
+| -------------- | ------------------------------------------------------------------- |
+| PickUp         | If we can pick up the item                                          |
+| Rigidbody      | The Rigidbody used for the physics                                  |
+| Distance       | The distance required for you to grab the item                      |
+| ThrowForce     | How much force is added onto the physics once you release the item  |
+
+## Disclaimer
+This product is not affiliated with Gorilla Tag or Another Axiom LLC and is not endorsed or otherwise sponsored by Another Axiom LLC. Portions of the materials contained herein are property of Another Axiom LLC. © 2021 Another Axiom LLC.
